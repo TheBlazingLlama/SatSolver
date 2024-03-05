4f818de1aa20b90b722553681b974ec65b46ee15
Cassius Ali Garcia <cassiusgarcia1@gmail.com>
2024-03-04 20:18:40 -0500

    Redid some of the functions to work with new format.

--- satSolver.py
+++ satSolver.py
@@ -43,7 +43,6 @@ def generate_cnf():
     num_minterms = 0
     file_name = input("Input the file you would like to read from: ")
     lines = get_lines_cleaned(file_name)
-    print(lines)
     cnf = []
     for line in lines:
         if is_comment(line):
@@ -64,7 +63,6 @@ def generate_cnf_value_based():
     num_minterms = 0
     file_name = input("Input the file you would like to read from: ")
     lines = get_lines_cleaned(file_name)
-    print(lines)
     cnf = []
     for line in lines:
         if is_comment(line):
@@ -73,7 +71,6 @@ def generate_cnf_value_based():
             num_vars = int(line[2])
             num_minterms = int(line[3])
         else:
-            #cube = generate_cube(line, num_vars)
             line = list(map(int, line))
             cnf.append(line)
 
@@ -96,12 +93,12 @@ def generate_cube(line, num_var):
 
 def create_clause_set(num_minterms, num_vars):
     set_of_clauses = []
-
+    '''
     for i in range(num_minterms):
         set_of_clauses.append([0])
         for j in range(num_vars-1):
             set_of_clauses[i].append(0)
-
+    '''
     return set_of_clauses
 
 # Returns varible to split on. When calling backtrack method call as such - backtrack(cnf, jersolow_wang(cnf))
@@ -113,46 +110,31 @@ def create_clause_set(num_minterms, num_vars):
 def jersolow_wang_2_sided_method(cnf):
     literal_weight = defaultdict(int)
     for clause in cnf:
-        for index, literal in enumerate(clause, start=1):
-            if literal == 1:
-                literal_weight[index] += 2 ** -len(clause)
-            elif literal == 2:
-                literal_weight[-index] += 2 ** -len(clause)
-            # For 0, indicating the variable doesn't appear in the clause
-            elif literal == 0:
-                pass
-            # Add more conditions if needed for other values
-    return max(literal_weight, key=literal_weight.get)+1
+        for literal in clause:
+            literal_weight[abs(literal)] += 2** -len(clause)
+    return max(literal_weight, key=literal_weight.get)
 
 # Somewhat standard among most DPLL implementations, input is cnf and the splitting variable, output is new cnf
-def bcp(cnf, unit):
-    modified = []
-    #if flag == 1:
-    #  polarity = 1
-    #  cnf_p = deepcopy(cnf)
-    #  for clause in cnf_p:
-    #    clause[unit] = 1
-    # Add functionality here
+def bcp(cnf, literal):
+    calculated = []
+    
     for clause in cnf:
-        if clause[unit] == 1:
+        if literal in clause:
             continue
-        if clause[unit] == 2:
-            clause[unit] = 0
-            new_clause = clause
+        if -literal in clause:
+            new_clause = []
+
+            for x in clause:
+                if x != -literal:
+                    new_clause.append(x)
+
             if not new_clause:
                 return -1
-            modified.append(new_clause)
+            calculated.append(new_clause)
         else:
-            #modified_clause = []
-            #for literal in clause:
-            #    if literal == unit:
-            #        modified_clause.append(1 if polarity == 'positive' else 2)
-            #    else:
-            #        modified_clause.append(0)
-            #modified.append(modified_clause)
-            modified.append(clause)
-    #print(modified)
-    return modified
+            calculated.append(clause)
+    #print(calculated)
+    return calculated
 
 def if_one_literal(clause):
     count_one = 0
@@ -177,35 +159,35 @@ def unit_propagation(cnf):
     row = 0
     assignment = []
     unit_clauses = []
+
+    for clause in cnf:
+        if len(clause) == 1:
+            unit_clauses.append(clause)
     
     for clause in cnf:
       if if_one_literal(clause)==1:
         unit_clauses.append(clause)
-        row += 1
         
     while unit_clauses:
-        #print(f'unit_clause: {unit_clauses}')
-        unit = unit_clauses[0]
-        #print(f'unit: {unit}')
-        index = unit_index(unit)
-        #print(f'index: {index}')
+        literal = unit_clauses[0]
+        index = literal[0]
         cnf = bcp(cnf, index)
-        assignment += [unit]
-        #print(f'cnf: {cnf}')
-        #print(f'row: {row}')
+        assignment += [literal]
         if clauses_unsat(cnf): #cnf == -1:
             return -1, []
         if clauses_all_one(cnf):#not cnf:
             return cnf, assignment
         unit_clauses = []
         for clause in cnf:
-          if if_one_literal(clause)==1:
+          if len(clause)==1:
             unit_clauses.append(clause)
-            row += 1
-    return cnf, assignment, row
+    return cnf, assignment
 
 # Check if all clauses evaluate to 1 (if set of clauses is empty)
 def clauses_all_one(set_of_clauses):
+
+    if isinstance(set_of_clauses,int):
+        return 0
 
     if len(set_of_clauses) == 0:
         return 1
@@ -213,40 +195,24 @@ def clauses_all_one(set_of_clauses):
         return 0
     
 def all_dc(clause):
-    try:
-        for val in clause:
-            if val != clause[0]:
-                return 0
+    if not clause:
         return 1
-    except:
-        if clause == 0:
-            return 1
     return 0
 
 # Check if any clause evaluates to 0
 def clauses_unsat(set_of_clauses):
-
-    for clause in set_of_clauses:
-        unsat = 1
-        if all_dc(clause):
-            unsat = 0
-        else:
-            for value in clause:
-                if value == 1:                            
-                    unsat = 0
-
-        if unsat:
-            return 1
-
+    if set_of_clauses == -1:
+        return 1
     return 0
 
 def dpll(cnf, set_of_clauses):
     
     # Call Unit Proagation to perform BCP.
-    cnf, unit_assignment, row = unit_propagation(cnf)
+    cnf, unit_assignment= unit_propagation(cnf)
+    set_of_clauses.append(unit_assignment)
+
     print(f'cnf: {cnf}')
     print(f'unit_assignment: {unit_assignment}')
-    set_of_clauses[row] = unit_assignment[0]
     print(f'soc: {set_of_clauses}')
     # If the clauses all simplify to 1
     if clauses_all_one(cnf):
@@ -264,11 +230,9 @@ def dpll(cnf, set_of_clauses):
     # pool.multiprocessing.Pool()
     # pool.multiprocessing.Pool(processes=2)
     # Doesn't work
-    set_of_clauses[variable] = 1
-    solution = dpll(bcp(cnf, variable), set_of_clauses)
+    solution = dpll(bcp(cnf, variable), set_of_clauses + [variable])
     if not solution:
-        set_of_clauses[variable] = 2
-        solution = dpll(bcp(cnf, variable), set_of_clauses)
+        solution = dpll(bcp(cnf, variable), set_of_clauses + [variable])
     return solution
 
 
@@ -281,7 +245,9 @@ if __name__ == "__main__":
     # print(set_of_clauses)
 
     # perform calculation
-    if dpll(cnf, set_of_clauses):
-        print("SAT")
+    result = dpll(cnf, set_of_clauses)
+    if result:
+        print("SATISFIABLE")
+        print(result)
     else:
-        print("UNSAT")
+        print("UNSATISFIABLE")
